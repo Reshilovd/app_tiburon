bc02c2dc54d94dcf777a312612bb349443308af5
Reshilovd <reshilov_d@mail.ru>
2025-06-04 17:26:27 +0300

    удалил лишние тестовые файлы

--- tetet.py
+++
@@ -1,6 +0,0 @@
-import json
-
-with open("\\\\10.165.134.71\\Qst\\dp726\\reshilov\\projects_tib_ip.json", encoding='utf-8') as json_file:
-    cc_projects = json.load(json_file)
-
-print(cc_projects)
